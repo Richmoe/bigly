20b282d9050dbd426e27dc20176f262916e7dc13
richmoe <richmoe@hotmail.com>
2019-04-13 21:44:59 -0700

    Add a Vertical Text style

--- components/StyledText.js
+++ components/StyledText.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export class MonoText extends React.Component {
   render() {
     return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />;
+  }
+}
+
+export class VText extends React.Component {
+  render() {
+    return <View style={[this.props.style, {justifyContent: 'center'}]}><Text>{this.props.children}</Text></View>;
   }
 }
